5c9b096f7a2ac0c7912c956bdd240e7b4703e9d9
iamzub.in <zchoudhary.10@gmail.com>
2024-10-05 02:39:30 +0530

    drag drop working?

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,34 @@
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[cfg(desktop)]
 mod tray;
 
 #[tauri::command]
-fn start_drag(file_path: String) -> Result<(), String> {
-    // Implement the OS-specific drag operation here
-    // This is a placeholder and needs to be implemented based on the OS
-    println!("Starting drag for file: {}", file_path);
+fn start_drag(app: tauri::AppHandle, file_path: String) -> Result<(), String> {
+
+    let item = match std::fs::canonicalize("../icons/icon.png") {
+        Ok(path) => drag::DragItem::Files(vec![path]),
+        Err(e) => {
+            eprintln!("Error finding icon file: {}", e);
+            return Ok(());
+        }
+    };
+    let window = app.get_webview_window("main").unwrap();
+    // Define the on_drop_callback function
+    let on_drop_callback = |result: drag::DragResult, _: drag::CursorPosition| {
+        println!("Drag result: {:?}", result);
+    };
+
+    // Start the drag operation
+    drag::start_drag(
+        window.ns_window().expect("Failed to get ns_window"),
+        item,
+        on_drop_callback,
+        drag::Options::default(),
+    ).expect("Failed to start drag operation");
+
+    println!("Starting drag for file sss: {}", file_path);
     Ok(())
 }
 
@@ -25,6 +47,7 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![start_drag, start_multi_drag])
         .setup(|app| {
+
             #[cfg(all(desktop))]
             {
                 let handle = app.handle();
