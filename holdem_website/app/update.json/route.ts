4f71008733079f1599328498692adedd38ecbd31
iamzubin <zchoudhary.10@gmail.com>
2025-07-28 17:55:02 +0530

    move update.json to route

---
+++ holdem_website/app/update.json/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+
+    const updateData = {
+        "version": "0.2.2",
+        "notes": "Release notes for Holdem 0.2.2",
+        "pub_date": "2025-07-01T12:24:40.987Z",
+        "platforms": {
+          "windows-x86_64": {
+            "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRWUt1Nm91NG90ZVBLWTA2bDFSWTgxMzBDZTFOVE5RRi8zZnQ1Sy9XTkJkcURtTzU5dXNuVllTZHJUYVhnbVZBWGxselVJdnVTVml1bHpuWTRrVWZnK1BXR0N2S3haNmc4PQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzUxMzcyMzEzCWZpbGU6aG9sZGVtXzAuMi4yX3g2NC1zZXR1cC5leGUKUnBndWJVenFVNjJncjRTSnFlSnF2L0k3T0ZpNFVSaHp4Yno5SUVrbVNBWkpkMTVuenlHUVFMVnppTmVQa29ranJHQUx5Q3dGblVUcWpZZzRucm5xQmc9PQo=",
+            "url": "https://github.com/iamzubin/holdem/releases/download/0.2.2/holdem_0.2.2_x64-setup.exe"
+          }
+        }
+      };
+
+  return NextResponse.json(updateData);
+} 
