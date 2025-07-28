ef88ed772cab93acced1560ba129d78328bc61f1
iamzubin <zchoudhary.10@gmail.com>
2025-07-28 23:45:01 +0530

    update app version

--- holdem_website/app/update.json/route.ts
+++ holdem_website/app/update.json/route.ts
@@ -3,16 +3,16 @@ import { NextResponse } from 'next/server';
 export async function GET() {
 
     const updateData = {
-        "version": "0.2.2",
-        "notes": "Release notes for Holdem 0.2.2",
-        "pub_date": "2025-07-01T12:24:40.987Z",
-        "platforms": {
-          "windows-x86_64": {
-            "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRWUt1Nm91NG90ZVBLWTA2bDFSWTgxMzBDZTFOVE5RRi8zZnQ1Sy9XTkJkcURtTzU5dXNuVllTZHJUYVhnbVZBWGxselVJdnVTVml1bHpuWTRrVWZnK1BXR0N2S3haNmc4PQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzUxMzcyMzEzCWZpbGU6aG9sZGVtXzAuMi4yX3g2NC1zZXR1cC5leGUKUnBndWJVenFVNjJncjRTSnFlSnF2L0k3T0ZpNFVSaHp4Yno5SUVrbVNBWkpkMTVuenlHUVFMVnppTmVQa29ranJHQUx5Q3dGblVUcWpZZzRucm5xQmc9PQo=",
-            "url": "https://github.com/iamzubin/holdem/releases/download/0.2.2/holdem_0.2.2_x64-setup.exe"
-          }
+      "version": "0.2.3",
+      "notes": "Release notes for Holdem 0.2.3",
+      "pub_date": "2025-07-28T18:12:36.669Z",
+      "platforms": {
+        "windows-x86_64": {
+          "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRWUt1Nm91NG90ZUtIL1JGbTlGTDIxZ0wwQzluaDBocHJKclpRK3BrK0RtanBkcXhyTDB5cThSK1AxMUt0bUY0QmJoV0ZOQkZSNU9HUGRnV0NIQTRJOGYwaGxXSDVHMVFvPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzUzNzI2MTU3CWZpbGU6aG9sZGVtXzAuMi4yX3g2NC1zZXR1cC5leGUKaU1rK3VoUnNYUHp4cnNOZXAzY29HVGgrRmhqcGE2SEpyS2djRk5hUXR5a2s3T0twRi9Mdmg5MU1aclV4eGZlMERIQkNFMlV1Zmg2cHhwZXpzWW4wRHc9PQo=",
+          "url": "https://github.com/iamzubin/holdem/releases/download/0.2.3/holdem_0.2.3_x64-setup.exe"
         }
-      };
+      }
+    };
 
   return NextResponse.json(updateData);
 } 
