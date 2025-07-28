7d04a17973991d4ccb6abff9319f4ca7354b0be6
iamzubin <zchoudhary.10@gmail.com>
2025-07-29 00:58:27 +0530

    update json

--- holdem_website/app/update.json/route.ts
+++ holdem_website/app/update.json/route.ts
@@ -5,10 +5,10 @@ export async function GET() {
     const updateData = {
       "version": "0.2.3",
       "notes": "Release notes for Holdem 0.2.3",
-      "pub_date": "2025-07-28T18:12:36.669Z",
+      "pub_date": "2025-07-28T19:25:39.317Z",
       "platforms": {
         "windows-x86_64": {
-          "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRWUt1Nm91NG90ZUtIL1JGbTlGTDIxZ0wwQzluaDBocHJKclpRK3BrK0RtanBkcXhyTDB5cThSK1AxMUt0bUY0QmJoV0ZOQkZSNU9HUGRnV0NIQTRJOGYwaGxXSDVHMVFvPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzUzNzI2MTU3CWZpbGU6aG9sZGVtXzAuMi4yX3g2NC1zZXR1cC5leGUKaU1rK3VoUnNYUHp4cnNOZXAzY29HVGgrRmhqcGE2SEpyS2djRk5hUXR5a2s3T0twRi9Mdmg5MU1aclV4eGZlMERIQkNFMlV1Zmg2cHhwZXpzWW4wRHc9PQo=",
+          "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRWUt1Nm91NG90ZUZXRlpYZlk4WVZrZmZET0hhWlgyYWl5WEszSW1QWVJud204Z3N6Q3cyQzczS0hHaEJyN28vY1dhdWhHcS85dnVVQUdxTVltVE1SQ2w2TkY4WVJFcXcwPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzUzNzMwNzM5CWZpbGU6aG9sZGVtXzAuMi4zX3g2NC1zZXR1cC5leGUKK3JOY2xDdTVucWQxdk1uemQvQ1dGWjJOeUNNT2hXK1I5cFVKWmk1QVBwVFJqb3FQZlhYZEJIVFdrQ09XQ2ZCSTlZNlpPMk54bTVQTDJCTTdIWCtuQVE9PQo=",
           "url": "https://github.com/iamzubin/holdem/releases/download/0.2.3/holdem_0.2.3_x64-setup.exe"
         }
       }
