64a9119d8614197fa8b233ac01c3090773b607af
Zubin Choudhary <zchoudhary.10@gmail.com>
2026-03-27 04:30:06 +0530

    feat: Add a new Holdem demo component, update dependencies, and integrate it into the application layout.

--- holdem_website/app/layout.tsx
+++ holdem_website/app/layout.tsx
@@ -78,7 +78,7 @@ export default async function RootLayout({
           defaultTheme="system"
         >
           <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
-            <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
+            <div className="relative mx-auto w-full max-w-[90vw] flex-1 px-4 pt-20">
               {/* <Header /> */}
               {children}
               <Footer />
